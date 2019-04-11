9f81cf0fc296884c46ad9c0fb20042eabc7517c7
forewei <597082933@qq.com>
2019-04-11 17:31:17 +0800

    add spring内部的后置处理器添加注释

---
+++ spring-cglib/src/main/java/com/forewei/service/Test1Service.java
@@ -0,0 +1,8 @@
+package com.forewei.service;
+
+/**
+ * @Author forewei
+ * @date 2019-4-8 11:38
+ */
+public class Test1Service {
+}
---
+++ spring-cglib/src/main/java/com/forewei/service/TestService.java
@@ -0,0 +1,8 @@
+package com.forewei.service;
+
+/**
+ * @Author forewei
+ * @date 2019-4-8 11:37
+ */
+public class TestService {
+}
